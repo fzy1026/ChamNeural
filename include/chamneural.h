44182ae122c8896e27bf21bdb5fa1204468c5692
cham <fan314g159@163.com>
2026-03-05 17:38:38 +0800

    补充一部分常用函数及其导数

--- include/chamneural.h
+++ include/chamneural.h
@@ -8,6 +8,11 @@ double QuadraticLoss(double ans,double output);// 二次损失函数
 double CrossEntropyLoss(int ans,Matrix output);// 交叉熵损失函数
 double HingeLoss(bool ans,double output);// Hinge损失函数
 double Logistic(double x);// Logistic函数
+double DLogistic(double x);// Logistic函数的导数
+Matrix Softmax(Matrix x);// Softmax函数
+Matrix Exp(Matrix x);// 矩阵的指数函数
+Matrix Diag(Matrix x);// 将一个列向量变为对角矩阵或提取一个矩阵的对角线元素组成一个列向量
+double LeakyReLU(double x,double gamma);// LeakyReLU函数
 
 
 
--- src/chamneural.cpp
+++ src/chamneural.cpp
@@ -20,3 +20,67 @@ double Logistic(double x)
 {
     return 1/(1+exp(-x));
 }
+
+double DLogistic(double x)
+{
+    double logistic = Logistic(x);
+    return logistic * (1-logistic);
+}
+
+Matrix Exp(Matrix x)
+{
+    Matrix ans(x.height, x.width);
+    for (int i = 0; i < x.height; i++)
+    {
+        for (int j = 0; j < x.width; j++)
+        {
+            ans.data[i][j] = exp(x.data[i][j]);
+        }
+    }
+    return ans;
+}
+
+Matrix Softmax(Matrix x)
+{
+    if(x.width != 1)
+    {
+        throw "Error: Softmax function only accepts column vector.";
+    }
+    Matrix One = Vector(vector<double>(x.height, 1));
+    return Exp(x)*(One*Exp(x)).Inverse();
+}
+
+Matrix Diag(Matrix x)
+{
+    if(x.width != 1)
+    {
+        if(x.height != x.width)
+        {
+            throw "Error: Diag function only accepts column vector or square matrix.";
+        }
+        else
+        {
+            Matrix ans(x.height, 1);
+            for (int i = 0; i < x.height; i++)
+            {
+                ans.data[i][0] = x.data[i][i];
+            }
+            return ans;
+        }
+    }
+    else
+    {
+        Matrix ans(x.height, x.height);
+        ans.SetZero();
+        for (int i = 0; i < x.height; i++)
+        {
+            ans.data[i][i] = x.data[i][0];
+        }
+        return ans;
+    }
+}
+
+double LeakyReLU(double x, double gamma = 0.01)
+{
+    return (x > 0) ? x : gamma * x;
+}
